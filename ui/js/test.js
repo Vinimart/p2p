5d5b2daa2300b6cd6999e48f0602b62253ceb4b8
Vinicius Martins <contato@vinimartdev.com>
2020-04-13 22:49:10 -0300

    Add componentes Photo Editor

---
+++ ui/js/test.js
@@ -0,0 +1,15 @@
+let lists = []
+
+let listName = 's1'
+
+let list = {}
+
+list.keeperId = 'idImgName1'
+list.cssToToggle = 'sel-list-item'
+
+lists[listName] = list
+
+console.log(listName.length)
+
+
+
